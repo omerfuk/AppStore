0585114ae955210b41dfcf245053bb3d9a9aa75a
Ömer Faruk Kılıçaslan <omerrr_2001@hotmail.com>
2022-07-13 22:08:29 +0300

    collectionView basic cells created, programmatic foundation prepared successfully

---
+++ AppStore/FeaturedAppsController.swift
@@ -0,0 +1,59 @@
+//
+//  ViewController.swift
+//  AppStore
+//
+//  Created by Ömer Faruk Kılıçaslan on 13.07.2022.
+//
+
+import UIKit
+
+class FeaturedAppsController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
+
+    private let cellId = "cellId"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        collectionView.backgroundColor = .white
+        
+        collectionView.register(CategoryCell.self, forCellWithReuseIdentifier: cellId)
+    }
+    
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath) as! CategoryCell
+        
+        return cell
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width, height: 150)
+    }
+
+
+}
+
+class CategoryCell: UICollectionViewCell {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setupViews()
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setupViews() {
+        contentView.backgroundColor = .black
+        
+        
+    }
+    
+}
